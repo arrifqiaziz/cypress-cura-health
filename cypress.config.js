6cbdd223b2e9bfbb356748d734e6ca2f63e10ab2
arrifqiaziz <arrifqiaziz@gmail.com>
2023-09-06 20:59:19 +0700

    add screenshot and video record

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://katalon-demo-cura.herokuapp.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-});
+    baseUrl: 'https://katalon-demo-cura.herokuapp.com',
+    defaultCommandTimeout: 5000,
+    video: true,
+    screenshotOnRunFailure: true
+  }
+})
--- cypress/e2e/cura-healthcare/login.cy.js
+++ cypress/e2e/cura-healthcare/login.cy.js
@@ -1,15 +1,25 @@
 describe('Login to application', () => {
   beforeEach(() => {
-    cy.visit('https://katalon-demo-cura.herokuapp.com/')
+    cy.visit('/')
   })
 
   it.only('Login with valid username and valid password', () => {
-    cy.get('#menu-toggle').click().should('be.visible')
-    cy.contains('Login').click().should('be.visible')
-    cy.get('#txt-username').clear().type('John Doe')
-    cy.get('#txt-password').clear().type('ThisIsNotAPassword')
-    cy.get('#btn-login').click()
+    cy.get('#menu-toggle').should('be.visible').click()
+    cy.contains('Login').should('be.visible').click()
+    cy.get('#txt-username')
+      .clear()
+      .should('be.visible')
+      .scrollIntoView()
+      .type('John Doe')
+    cy.get('#txt-password')
+      .clear()
+      .should('be.visible')
+      .scrollIntoView()
+      .type('ThisIsNotAPassword')
+    cy.screenshot()
+    cy.get('#btn-login').should('be.visible').click()
     cy.url().should('contain', '#appointment')
+    cy.screenshot()
   })
 
   it('Login with valid username and invalid password', () => {})
