ffbe1cb7c0b3d3321e795f873d541ca47b967d7a
arrifqiaziz <arrifqiaziz@gmail.com>
2023-09-06 20:08:37 +0700

    update

--- cypress/e2e/cura-healthcare/makeAppoinment.cy.js
+++ cypress/e2e/cura-healthcare/makeAppoinment.cy.js
@@ -1,18 +1,14 @@
 describe('Make Appointment', () => {
 
   it('Make appointment - not yet login', () => {
-    cy.visit('https://example.cypress.io')
   })
   
   it('Make appointment - already login', () => {
-    cy.visit('https://example.cypress.io')
   })
   
   it('Make appointment - not yet login', () => {
-    cy.visit('https://example.cypress.io')
   })
   
   it('Make appointment - not yet login', () => {
-    cy.visit('https://example.cypress.io')
   })
 })
